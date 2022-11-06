fa4355112266c7a50e2f7c7aa51ffde1bfdd8560
Yuliya Buday <yuliya.buday@gmal.com>
2022-11-06 18:08:33 +0300

    Add homework2

--- month/month/urls.py
+++ month/month/urls.py
@@ -1,4 +1,4 @@
-"""first URL Configuration
+"""month URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from seasons import views
-urlpatterns = [
-    #path('admin/', admin.site.urls),
-    path('<seasons/>', views.index),
+from description.views import name_of_month
 
-]
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('<str:name_of_month>/', name_of_month),
+
+    ]
